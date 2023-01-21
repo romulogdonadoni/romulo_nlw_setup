4c1ebfbba6935e5c74d419ac86ea8623f3038a38
romulogdonadoni <romulogdonadoni@gmail.com>
2023-01-21 07:26:10 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+const form = document.querySelector("#form-habits")
+const nlwSetup = new NLWSetup(form)
+const button = document.querySelector("header button")
+
+button.addEventListener("click", add)
+form.addEventListener("change", save)
+function add() {
+  const today = new Date().toLocaleDateString("pt-br").slice(0, -5)
+  const dayExists = nlwSetup.dayExists(today)
+  if (dayExists) {
+    return
+  }
+  nlwSetup.addDay(today)
+}
+function save() {
+  localStorage.setItem("NLWSetup@Habits", JSON.stringify(nlwSetup.data))
+}
+//const data = {
+//  run: ["01-01", "01-02", "01-03", "01-04", "01-05", "01-06", "01-07", "01-08"],
+//}
+const data = JSON.parse(localStorage.getItem("NLWSetup@Habits")) || {}
+nlwSetup.setData(data)
+nlwSetup.load()
